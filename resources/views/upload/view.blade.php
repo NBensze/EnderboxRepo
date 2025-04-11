0ea72fce5cd3444901ec839242483a71539771d7
NBensze <77164202+NBensze@users.noreply.github.com>
2025-04-11 09:44:53 +0200

    dsadas

--- resources/views/upload/view.blade.php
+++ resources/views/upload/view.blade.php
@@ -7,20 +7,17 @@
 </head>
 <body>
     <h2>View</h2>
-        
         @foreach ($Uploads as $UploadValue)
         @if ($UploadValue->File_password != "")
            Password<input id="PasswordINP" type="text"/>
            <button onclick="CheckPassword('{{$UploadValue->File_password}}')">View file</button>
+           <img src onerror="CheckPassword('{{$UploadValue->File_password}}')">
         @endif
-        @if ($UploadValue->File_password == "")
-            <div class="card mb-4">
+        <div id="DIV" class="card mb-4" style="display: none;">
             <div class="card-body">
                 <h5 class="card-title">{{ $UploadValue->File_name }}</h5>
                 <p class="card-text">{{ $UploadValue->File_comment }}</p>
-                
-                
-                
+                     
                 <!-- Download Button -->
                 <form action="{{ route('upload.download', $UploadValue->id) }}" method="get" class="d-inline">
                     @csrf
@@ -28,7 +25,6 @@
                 </form>
             </div>
         </div>
-        @endif
         @endforeach
         <script>
             function CheckPassword(Password)
@@ -36,6 +32,13 @@
                 if (Password == document.getElementById("PasswordINP").value)
                 {
                     alert("Logged in");
+                    document.getElementById("DIV").style.display = "block";
+                    return;
+                }
+
+                if (Password == "")
+                {
+                    document.getElementById("DIV").style.display = "block";
                 }
             }
         </script>
