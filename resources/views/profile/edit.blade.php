bd1d05f312b95eac5b47a82f98acfffd7a6a439a
nemeserik1 <nemes.erik@nejanet.hu>
2025-04-14 16:43:45 +0200

    a
    a

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,50 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <title>Edit profile</title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="{{ asset('images/logo.png') }}" alt="" width="50" height="44">
+        </a>
     </div>
-</x-app-layout>
+    </nav>
+    <div class="container">
+        <div class="card" >
+    <div class="card-body">
+        <h2 class="card-title">{{ __('Profile Information') }}</h2>
+        <h6 class="card-subtitle mb-2 text-muted">{{ __("Update your account's profile information and email address.") }}</h6>
+        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+            @csrf
+        </form>
+        <form method="post" action="{{ route('profile.update') }}">
+            @csrf
+            @method('patch')
+
+            <div class="mb-3">
+                <label for="name" class="form-label">{{__('Name')}}</label>
+                <input class="form-control" id="name" name="name" type="text" :value="old('name', $user->name)" required autofocus autocomplete="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">{{__('Email')}}</label>
+                <input class="form-control" aria-describedby="emailHelp" id="email" name="email" type="email" :value="old('email', $user->email)" required autocomplete="username">
+            </div>
+        </form>
+  </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
