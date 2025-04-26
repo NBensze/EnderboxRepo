3ff6c1156d591a03d558b9397f8852bc8da544cd
NBensze <77164202+NBensze@users.noreply.github.com>
2025-04-26 14:07:07 +0200

    dasda

--- resources/views/upload/view.blade.php
+++ resources/views/upload/view.blade.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
-    <h2>View</h2>
+    <h2>View file</h2>
+<!--Egy cardba ez egeszet kozepre-->
         @foreach ($Uploads as $UploadValue)
         @if ($UploadValue->File_password != "")
         <div id="PasswordDIV" class="form-group">
